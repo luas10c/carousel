f3fd1b5dd35e572820b4e16d82174fdf16689522
Luciano Alves <luas10c@gmail.com>
2023-08-01 14:50:27 -0300

    chore: first commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type{import('prettier').Config}*/
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  arrowParens: 'always',
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 90,
+  plugins: []
+}
